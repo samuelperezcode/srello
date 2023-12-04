e2ae43a4118f70bdfb7fecfc294f33a1a6f6986d
SamyCode <sambea2103@gmail.com>
2023-12-04 11:56:31 -0500

    Create sign-in and sign-up

--- src/app/(platform)/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(platform)/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
  
-export default function Page() {
-  return <SignUp />;
+export default function SignInPage() {
+  return <SignIn />;
 }
